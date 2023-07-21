4850dab3114d1b4a5ae7f8c5a164bb24a0f8c51d
FizaFahameenM <131661097+FizaFahameenM@users.noreply.github.com>
2023-07-21 23:13:06 +0530

    Add files via upload

---
+++ Difference 2 nums.c
@@ -0,0 +1,10 @@
+// program to print DIFFERENCE of 2 numbers
+#include <stdio.h>
+int main()
+{
+    int a,b,sub;
+    a=5;b=10;
+    printf("%d %d\n",a,b);
+    sub = a-b;
+    printf("The DIFFERENCE is %d",sub);
+}
