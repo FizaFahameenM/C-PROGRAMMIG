249efb1226e2a7ff9187f539d85e30084f222c9f
FizaFahameenM <131661097+FizaFahameenM@users.noreply.github.com>
2023-07-21 23:10:34 +0530

    Add files via upload
    SUM OF 2 NUMS

---
+++ Sum 2 nums.c
@@ -0,0 +1,10 @@
+// program to print SUM of 2 numbers
+#include <stdio.h>
+int main()
+{
+    int a,b,sum;
+    a=5;b=10;
+    printf("%d %d\n",a,b);
+    sum = a+b;
+    printf("The Sum is %d",sum);
+}
